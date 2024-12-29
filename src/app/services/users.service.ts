d29959a644c8be6150d3cda42cb14fd941911cb9
Brian Ganster <BGanster96@gmail.com>
2024-12-29 11:57:19 -0800

    Saved user list to local storage

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -6,14 +6,34 @@ import { User } from '../models/user';
 })
 export class UsersService {
 
+  readonly usersKey = 'users';
   users: User[] = [];
 
-  constructor() { }
+  constructor() {
+    const cache = localStorage.getItem(this.usersKey);
+    console.log('cache:', cache);
+    if (cache) {
+      const userCache:User[]|undefined = JSON.parse(cache);
+      console.log('parsed cache:', userCache);
+      if (userCache)
+        this.users = userCache;
+    }
+  }
 
-  addUsers = (emails: string[]) =>
+  addUsers(emails: string[]) {
     this.users.push(...emails.map(e => ({
       email: e,
       maxSkips: 5,
       remainingSkips: 0
     })));
+    this.setStorage();
+  }
+
+  // eventually this will migrate to
+  // a generic storage serice
+  private setStorage = () =>
+    localStorage.setItem(
+      this.usersKey,
+      JSON.stringify(this.users)
+    );
 }
