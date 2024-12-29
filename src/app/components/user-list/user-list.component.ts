a6566d9fdbfcad7af61f8c1703b9cb17d6404064
Brian Ganster <BGanster96@gmail.com>
2024-12-28 19:15:08 -0800

    Contained user list within a list

--- src/app/components/user-list/user-list.component.ts
+++ src/app/components/user-list/user-list.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { UsersService } from '../../services/users.service';
 import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-user-list',
   imports: [
     MatSidenavModule,
     MatButtonModule,
+    MatListModule,
   ],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.scss'
@@ -15,7 +18,8 @@ import { MatButtonModule } from '@angular/material/button';
 export class UserListComponent {
 
   constructor(
-    public userServ: UsersService
+    public router: Router,
+    public userServ: UsersService,
   ) { }
 
 }
